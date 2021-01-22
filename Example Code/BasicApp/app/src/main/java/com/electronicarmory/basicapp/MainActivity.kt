66d35024a2fe3b79ee1a04bb5720c192fc752f65
Mike Ziray <mziray@gmail.com>
2021-01-21 19:39:34 -0700

    Adding example code for basic Android app.

---
+++ Example Code/BasicApp/app/src/main/java/com/electronicarmory/basicapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.electronicarmory.basicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.text = "Initial Text"
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Button pressed"
+            Log.d("BSU", "Button pressed")
+            answerView.visibility = View.VISIBLE
+        }
+    }
+}
