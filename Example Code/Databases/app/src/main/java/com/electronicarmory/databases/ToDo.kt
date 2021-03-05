0bc5be000acc404ee77396118ad81afad0cc5efc
Mike Ziray <mziray@gmail.com>
2021-03-04 20:41:17 -0700

    Adding class notes and example code for Databases and ObjectBox

---
+++ Example Code/Databases/app/src/main/java/com/electronicarmory/databases/ToDo.kt
@@ -0,0 +1,13 @@
+package com.electronicarmory.databases
+
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+
+@Entity
+data class ToDo(
+    @Id
+    var id:Long = 0,
+    var title:String = "New Todo",
+    var description:String,
+    val isDone:Boolean = false,
+    var dueDate:String)
