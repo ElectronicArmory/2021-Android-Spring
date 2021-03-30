7cc0a25b767c892c562d7c4361c0acaf280f2256
Mike Ziray <mziray@gmail.com>
2021-03-30 17:11:06 -0600

    Assigning Project 2. Adding class notes and updated Web Service example code with basic refresh button.

--- Example Code/WebServices/app/src/main/java/com/electronicarmory/webservices/MainActivity.kt
+++ Example Code/WebServices/app/src/main/java/com/electronicarmory/webservices/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.electronicarmory.webservices
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var currentPrice:String
+    lateinit var currentPrice: String
+    lateinit var refreshButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +25,13 @@ class MainActivity : AppCompatActivity() {
 
         val serviceDownloader = BitcoinPriceService()
 
-        val callback = object: Callback<BpiResponse> {
+        val callback = object : Callback<BpiResponse> {
             override fun onFailure(call: Call<BpiResponse>?, t: Throwable?) {
                 Log.e("MainActivity", "Problems", t)
             }
 
             override fun onResponse(call: Call<BpiResponse>?, response: Response<BpiResponse>?) {
-                response?.isSuccessful.let{
+                response?.isSuccessful.let {
                     Log.d("Response", response.toString())
                     Log.d("Response", response?.body().toString())
                     val resp = response?.body()?.bpi?.USD
@@ -46,6 +48,11 @@ class MainActivity : AppCompatActivity() {
 
         }
         serviceDownloader.getBitcoinPrice(callback)
+
+        refreshButton = findViewById(R.id.refreshButton)
+        refreshButton.setOnClickListener {
+            serviceDownloader.getBitcoinPrice(callback)
+        }
 
     }
 }
